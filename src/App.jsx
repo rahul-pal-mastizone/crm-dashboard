10827b3acb5a25256884852404d65db290473718
Rahul Pal <rahulpal.moderntechno@gmail.com>
2025-07-30 10:36:51 +0530

    Implemented dashboard layout with private routes and post-login redirect

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
 import { Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
 import Home from './pages/Home';
 import Clients from './pages/Clients';
 import Projects from './pages/Projects';
-import Login from './pages/Login';
+import Dashboard from './components/Dashboard'; // layout wrapper
 import PrivateRoute from './PrivateRoute';
-import Dashboard from './components/Dashboard';
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
-      <Route path="/" element={<PrivateRoute><Home /></PrivateRoute>} />
-      <Route path="/clients" element={<PrivateRoute><Clients /></PrivateRoute>} />
-      <Route path="/projects" element={<PrivateRoute><Projects /></PrivateRoute>} />
-      <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+      
+      {/* Protected dashboard routes with layout */}
+      <Route
+        path="/"
+        element={
+          <PrivateRoute>
+            <Dashboard />
+          </PrivateRoute>
+        }
+      >
+        <Route index element={<Home />} />
+        <Route path="clients" element={<Clients />} />
+        <Route path="projects" element={<Projects />} />
+      </Route>
     </Routes>
   );
 }
